2feb09a733a183abc79440978b7c2e01bea13323
CarsonH19 <questline.carson@gmail.com>
2024-09-14 02:26:20 +0900

    feat: connect

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-
 const taskRoutes = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 //middleware - adds data to req.body
+
 app.use(express.json());
 
 // routes
@@ -21,4 +23,13 @@ app.use("/api/v1/tasks", taskRoutes);
 
 const port = 3000;
 
-app.listen(port, console.log(`server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
